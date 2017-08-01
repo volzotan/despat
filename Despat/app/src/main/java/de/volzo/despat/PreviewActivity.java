80c9bee17020f0f86018d862922ff1d63226eb7e
Christopher Getschmann <christopher@volzo.de>
2017-08-01 10:54:16 +0200

    preview activity created

---
+++ Despat/app/src/main/java/de/volzo/despat/PreviewActivity.java
@@ -0,0 +1,18 @@
+package de.volzo.despat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class PreviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview);
+
+//        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+//        imageView.setImageBitmap(res.bitmap);
+
+    }
+}
