53d22efbe6292559c5c89e7e890b9986efabb797
Christopher Getschmann <christopher@volzo.de>
2017-10-05 18:21:34 +0200

    servpat updated for image uploading

--- servpat/debug/send_image.py
+++ servpat/debug/send_image.py
@@ -3,8 +3,22 @@ from datetime import datetime
 import json
 
 URL = "http://localhost:5000/" + "image"
+DATEFORMAT_INPUT = "%Y-%m-%d %H:%M:%S.%f"
 
 files = {"file": open("test.jpg", "rb")}
 
-r = requests.post(URL, files=files)
-print(r)
+#r = requests.post(URL, files=files)
+#print(r)
+
+r = requests.Request('POST', URL, files=files, data={"deviceId": "1020", "timestamp": datetime.now().strftime(DATEFORMAT_INPUT)})
+req = r.prepare()
+
+print('{}\n{}\n{}\n\n{}'.format(
+    '-----------START-----------',
+    req.method + ' ' + req.url,
+    '\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
+    req.body,
+))
+
+s = requests.Session()
+s.send(req)
