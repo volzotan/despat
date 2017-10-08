05add9defddb910354d2e7710d8034fd6d9b3bfa
Christopher Getschmann <christopher@volzo.de>
2017-10-08 13:16:07 +0200

    Lifecycle: onTerminate

--- Despat/app/src/main/java/de/volzo/despat/Despat.java
+++ Despat/app/src/main/java/de/volzo/despat/Despat.java
@@ -1,11 +1,14 @@
 package de.volzo.despat;
 
 import android.app.Application;
+import android.util.Log;
 
 import de.volzo.despat.support.CameraAdapter;
 import de.volzo.despat.web.ServerConnector;
 
 public class Despat extends Application {
+
+    public static String TAG = Despat.class.getSimpleName();
 
     private CameraAdapter camera;
     private SystemController systemController;
@@ -21,6 +24,16 @@ public class Despat extends Application {
         ServerConnector serverConnector = new ServerConnector(this);
         serverConnector.sendEvent(ServerConnector.EventType.INIT, null);
 
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+
+        Log.i(TAG, "despat terminate.");
+
+        ServerConnector serverConnector = new ServerConnector(this);
+        serverConnector.sendEvent(ServerConnector.EventType.SHUTDOWN, null);
     }
 
     public void setCamera(CameraAdapter cameraAdapter) {
