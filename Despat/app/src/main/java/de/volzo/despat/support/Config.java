b49fc3a4be978c8fb9b1057466ea04c15eaab09f
Christopher Getschmann <christopher@volzo.de>
2018-02-20 17:03:48 +0100

    reformatted Config

--- Despat/app/src/main/java/de/volzo/despat/support/Config.java
+++ Despat/app/src/main/java/de/volzo/despat/support/Config.java
@@ -22,29 +22,29 @@ public class Config {
 
     public static final String TAG = Config.class.getSimpleName();
 
-    public static final String DATEFORMAT                       = "yyyy-MM-dd HH:mm:ss.SSS";
-    public static final String IMAGE_FILEEXTENSION              = ".jpg";
+    public static final String DATEFORMAT                           = "yyyy-MM-dd HH:mm:ss.SSS";
+    public static final String IMAGE_FILEEXTENSION                  = ".jpg";
 
-    public static final float IMGROLL_FREE_SPACE_THRESHOLD      = 300; // in MB
-    public static final boolean IMGROLL_DELETE_IF_FULL          = true;
+    public static final float IMGROLL_FREE_SPACE_THRESHOLD          = 300; // in MB
+    public static final boolean IMGROLL_DELETE_IF_FULL              = true;
 
-    public static final boolean PHONE_HOME                      = true;
+    public static final boolean PHONE_HOME                          = true;
 
-    public static final String SYNC_AUTHORITY                   = "de.volzo.despat.web.provider";
-    public static final String SYNC_ACCOUNT_TYPE                = "de.volzo.despat.servpat";
-    public static final String SYNC_ACCOUNT                     = "despatSync";
+    public static final String SYNC_AUTHORITY                       = "de.volzo.despat.web.provider";
+    public static final String SYNC_ACCOUNT_TYPE                    = "de.volzo.despat.servpat";
+    public static final String SYNC_ACCOUNT                         = "despatSync";
 
     // ---------------------------------------------------------------------------------------------
 
     // use CameraController v1 (old) or v2
-    public static final int USE_CAMERA_CONTROLLER               = 2;
+    public static final int USE_CAMERA_CONTROLLER                   = 2;
 
     // jpegs and/or raw | v2 only
-    public static final boolean FORMAT_JPG                      = true;
-    public static final boolean FORMAT_RAW                      = true;
+    public static final boolean FORMAT_JPG                          = true;
+    public static final boolean FORMAT_RAW                          = false;
 
     // display a preview on the main activity
-    public static final boolean START_CAMERA_ON_ACTIVITY_START  = false;
+    public static final boolean START_CAMERA_ON_ACTIVITY_START      = false;
 
     // close the camera without cancelling the
     // AF requests | v2 only
@@ -52,79 +52,84 @@ public class Config {
 
     // delay between starting AE measurement
     // and release | v1 only
-    public static final int SHUTTER_RELEASE_DELAY               = 500;
+    public static final int SHUTTER_RELEASE_DELAY                   = 500;
 
     // number of images taken during every capture
-    public static final int NUMBER_OF_BURST_IMAGES              = 2;
+    public static final int NUMBER_OF_BURST_IMAGES                  = 2;
 
     // use fixed ISO value. null=disabled | v1 only
-    public static final Integer FIXED_ISO_VALUE                 = 200;
+    public static final Integer FIXED_ISO_VALUE                     = 200;
 
     // over- or underexposure compensation | v1 only
     // array position is image number in burst sequence
     // if length==1 every image gets the value of [0]
-    public static final int[] EXPOSURE_COMPENSATION             = {0, -12};
+    public static final int[] EXPOSURE_COMPENSATION                 = {0, -12};
 
     // maximal time the AF/AE/AWB metering functions
     // may try to find a fix before shutter is
     // released anyway
-    public static final int METERING_MAX_TIME                  = 2000;
+    public static final int METERING_MAX_TIME                       = 2000;
+
+    // make captured images immediately available to
+    // the android gallery app | v2 only
+    public static final boolean RUN_MEDIASCANNER_AFTER_CAPTURE      = false;
 
     // ---------------------------------------------------------------------------------------------
 
     // ShutterService closes the camera after X seconds
-    public static final long SHUTTER_CAMERA_MAX_LIFETIME        = 6000;
+    public static final long SHUTTER_CAMERA_MAX_LIFETIME            = 8000;
 
     // ShutterService itself is closed after X seconds
-    public static final long SHUTTER_SERVICE_MAX_LIFETIME       = 8000;
+    public static final long SHUTTER_SERVICE_MAX_LIFETIME           = 8000;
 
     // the maximum time the wakelock is guaranteed to be active
-    public static final long WAKELOCK_MAX_LIFETIME              = 9000;
+    public static final long WAKELOCK_MAX_LIFETIME                  = 9000;
 
     // ---------------------------------------------------------------------------------------------
 
-    public static final String ACRA_REPORT_URL                  = "http://zoltep.de/report";
+    public static final String ACRA_REPORT_URL                      = "http://zoltep.de/report";
 
     // copy logcat output and write to file
-    public static final boolean BACKUP_LOGCAT                   = false;
+    public static final boolean BACKUP_LOGCAT                       = false;
 
     // redirect logcat output via -f to file
-    public static final boolean REDIRECT_LOGCAT                 = false;
+    public static final boolean REDIRECT_LOGCAT                     = false;
 
     // logcat text file directory
-    public static final File LOGCAT_DIR                         = new File(Environment.getExternalStorageDirectory(), ("despat"));
+    public static final File LOGCAT_DIR                             = new File(Environment.getExternalStorageDirectory(), ("despat"));
 
     // dateformat used for the logcat file name
-    public static final String DATEFORMAT_LOGFILE               = "yyyy-MM-dd";
+    public static final String DATEFORMAT_LOGFILE                   = "yyyy-MM-dd";
 
     // reboots the device if sudo rights are granted and a
     // critical error happened (usually the camera)
-    public static final boolean REBOOT_ON_CRITICAL_ERROR        = false;
+    public static final boolean REBOOT_ON_CRITICAL_ERROR            = false;
 
     // ---------------------------------------------------------------------------------------------
 
     // DEFAULT_SHUTTER_INTERVAL should not be shorter than 6s (5s is android minimum
     // and a few extra ms are needed for compensation of scheduling irregularities)
-    private static final long DEFAULT_SHUTTER_INTERVAL          = 10 * 1000; // in ms
-    private static final long DEFAULT_HEARTBEAT_INTERVAL        = 15 * 60 * 1000L; // Minimum interval is 15m
-    private static final long DEFAULT_UPLOAD_INTERVAL           = 15 * 60 * 1000L;
-    private static final File DEFAULT_IMAGE_FOLDER              = new File(Environment.getExternalStorageDirectory(), ("despat"));
-    private static final String DEFAULT_SERVER_ADDRESS          = "http://zoltep.de"; // format protocol://example.com
-    private static final boolean DEFAULT_RESUME_AFTER_REBOOT    = false;
-    private static final long DEFAULT_MIN_SYNC_INTERVAL         = 5 * 60 * 1000; // at most every X ms
+    private static final long DEFAULT_SHUTTER_INTERVAL              = 10 * 1000; // in ms
+    private static final long DEFAULT_HEARTBEAT_INTERVAL            = 15 * 60 * 1000L; // Minimum interval is 15m
+    private static final long DEFAULT_UPLOAD_INTERVAL               = 15 * 60 * 1000L;
+    private static final File DEFAULT_IMAGE_FOLDER                  = new File(Environment.getExternalStorageDirectory(), ("despat"));
+    private static final String DEFAULT_SERVER_ADDRESS              = "http://zoltep.de"; // format protocol://example.com
+    private static final boolean DEFAULT_RESUME_AFTER_REBOOT        = false;
+    private static final long DEFAULT_MIN_SYNC_INTERVAL             = 5 * 60 * 1000; // at most every X ms
 
-    private static final String SHAREDPREFNAME                  = "de.volzo.despat.DEFAULT_PREFERENCES";
+    private static final String SHAREDPREFNAME                      = "de.volzo.despat.DEFAULT_PREFERENCES";
 
-    public static final String KEY_DEVICENAME                   = "de.volzo.despat.deviceName";
-    public static final String KEY_SHUTTER_INTERVAL             = "de.volzo.despat.shutterInterval";
-    public static final String KEY_IMAGE_FOLDER                 = "de.volzo.despat.imageFolder";
-    public static final String KEY_PHONE_HOME                   = "de.volzo.despat.phoneHome";
-    public static final String KEY_SERVER_ADDRESS               = "de.volzo.despat.serverAddress";
-    public static final String KEY_HEARTBEAT_INTERVAL           = "de.volzo.despat.heartbeatInterval";
-    public static final String KEY_UPLOAD_INTERVAL              = "de.volzo.despat.uploadInterval";
-    public static final String KEY_RESUME_AFTER_REBOOT          = "de.volzo.despat.resumeAfterReboot";
-    public static final String KEY_LAST_SYNC                    = "de.volzo.despat.lastSync";
-    public static final String KEY_MIN_SYNC_INTERVAL            = "de.volzo.despat.minSyncInterval";
+    public static final String KEY_DEVICENAME                       = "de.volzo.despat.deviceName";
+    public static final String KEY_SHUTTER_INTERVAL                 = "de.volzo.despat.shutterInterval";
+    public static final String KEY_IMAGE_FOLDER                     = "de.volzo.despat.imageFolder";
+    public static final String KEY_PHONE_HOME                       = "de.volzo.despat.phoneHome";
+    public static final String KEY_SERVER_ADDRESS                   = "de.volzo.despat.serverAddress";
+    public static final String KEY_HEARTBEAT_INTERVAL               = "de.volzo.despat.heartbeatInterval";
+    public static final String KEY_UPLOAD_INTERVAL                  = "de.volzo.despat.uploadInterval";
+    public static final String KEY_RESUME_AFTER_REBOOT              = "de.volzo.despat.resumeAfterReboot";
+    public static final String KEY_LAST_SYNC                        = "de.volzo.despat.lastSync";
+    public static final String KEY_MIN_SYNC_INTERVAL                = "de.volzo.despat.minSyncInterval";
+    public static final String KEY_NEXT_SHUTTER_SERVICE_INVOCATION  = "de.volzo.despat.minSyncInterval";
 
     /*
     image folder
@@ -142,6 +147,9 @@ public class Config {
     */
 
     public static void init(Context context) {
+
+        // delete everything
+        // context.getSharedPreferences(SHAREDPREFNAME, 0).edit().clear().commit();
 
         // ...
     }
@@ -245,6 +253,13 @@ public class Config {
         editor.apply();
     }
 
+    private static void setProperty(Context context, String key, long value) {
+        SharedPreferences settings = context.getSharedPreferences(SHAREDPREFNAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putLong(key, value);
+        editor.apply();
+    }
+
     private static String getProperty(Context context, String key, String defaultValue) {
         SharedPreferences settings = context.getSharedPreferences(SHAREDPREFNAME, Context.MODE_PRIVATE);
         return settings.getString(key, defaultValue);
@@ -302,7 +317,7 @@ public class Config {
         return getPropertyLong(context, KEY_SHUTTER_INTERVAL, DEFAULT_SHUTTER_INTERVAL);
     }
 
-    public static void setShutterInterval(Context context, String shutterInterval) {
+    public static void setShutterInterval(Context context, long shutterInterval) {
         setProperty(context, KEY_SHUTTER_INTERVAL, shutterInterval);
     }
 
@@ -358,8 +373,16 @@ public class Config {
         return getPropertyLong(context, KEY_MIN_SYNC_INTERVAL, DEFAULT_MIN_SYNC_INTERVAL);
     }
 
-    public static void setMinSyncInterval(Context context, String minSyncInterval) {
+    public static void setMinSyncInterval(Context context, long minSyncInterval) {
         setProperty(context, KEY_MIN_SYNC_INTERVAL, minSyncInterval);
+    }
+
+    public static long getNextShutterServiceInvocation(Context context) {
+        return getPropertyLong(context, KEY_NEXT_SHUTTER_SERVICE_INVOCATION, -1);
+    }
+
+    public static void setNextShutterServiceInvocation(Context context, long timestamp) {
+        setProperty(context, KEY_NEXT_SHUTTER_SERVICE_INVOCATION, timestamp);
     }
 
     public static void validate() throws Exception {
