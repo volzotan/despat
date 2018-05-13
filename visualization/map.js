08be07090c8dbaf188cb03796c350d47a5bc3544
Christopher Getschmann <christopher@volzo.de>
2018-05-13 18:30:00 +0200

    visualization: canvas embedded in SVG

--- visualization/map.js
+++ visualization/map.js
@@ -1,11 +1,57 @@
-var session = {
-    "camera": {
-        "1": {
-            "name": "Moto Z",
-            "position": [50.971038, 11.038084]
-        },
-    }
+var dataset = {
 
+    "cameras": [
+        {
+            "id": "1",
+            "name": "Moto Z",
+            "position": [50.971040, 11.038093]
+        },
+        {
+            "id": "2",
+            "name": "ZTE",
+            "position": [50.970930, 11.038083]
+        }
+        ],
+
+    "corresponding_points": [
+        {
+            "id": "1",
+            "position": [50.971296, 11.037630]
+        },
+        {
+            "id": "2",
+            "position": [50.971173, 11.037914]
+
+        },
+        {
+            "id": "3",
+            "position": [50.971456, 11.037915]
+
+        },
+        {
+            "id": "4",
+            "position": [50.971705, 11.037711]
+
+        },
+        {
+            "id": "5",
+            "position": [50.971402, 11.037796]
+
+        },
+        {
+            "id": "5",
+            "position": [50.971636, 11.037486]
+
+        }
+    ],
+
+    "data": "boxes.txt",
+
+    "classmap": {
+        "1": "person",
+        "2": "car"
+
+    }
 };
 
 var pi = Math.PI,
@@ -14,23 +60,37 @@ var pi = Math.PI,
 var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height")
-    // margin = {top: 20, right: 20, bottom: 30, left: 40},
-    // g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    layer_map = svg.append("g");
-    layer_hex = svg.append("g");
-    // layer_sca = svg.append("g");
-    layer_sca = d3.select("canvas").node().getContext("2d");
-    layer_sym = svg.append("g");
+// margin = {top: 20, right: 20, bottom: 30, left: 40},
+// g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-console.log(layer_sca);
+    layer_map = svg.append("g");
+
+    layer_hex = svg.append("g");
+
+    foreignObject = svg.append("foreignObject")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", width)
+        .attr("height", height);
+    foBody = foreignObject.append("xhtml:body")
+        .style("margin", "0px")
+        .style("padding", "0px")
+        .style("background", "none")
+        .style("width", width + "px")
+        .style("height", height + "px");
+    canvas = foBody.append("canvas")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", width)
+        .attr("height", height);
+    layer_sca = canvas.node().getContext("2d");
+
+    layer_sym = svg.append("g");
 
 var projection = d3.geoMercator()
     .scale((1 << 8 + 19) / tau)
     .translate([width / 2, height / 2])
     .center([11.037630, 50.971296]);
-
-var path = d3.geoPath()
-    .projection(projection);
 
 var color = d3.scaleSequential(d3.interpolateLab("white", "steelblue"))
     .domain([0, 70]);
@@ -43,27 +103,43 @@ var hexbin = d3.hexbin()
     .radius(5)
     .extent([[0, 0], [width, height]]);
 
-var symbols_data = [
-    ["C", "Camera", [50.971038, 11.038084], "red"]
-];
-
-d3.text("boxes.txt", function(error, raw) {
+d3.text(dataset["data"], function(error, raw) {
     if (error) throw error;
+
+    var boxes = d3.dsvFormat("|").parseRows(raw);
+    boxes.forEach((box, index) => {
+        var coord = projection([box[5], box[4]]);
+        box[5] = coord[0];
+        box[4] = coord[1];
+        return boxes[index] = box; // lonlat!
+    });
+
+    buildGraph(dataset["cameras"], dataset["corresponding_points"], boxes, null);
+});
+
+$("li").click(function () {
+
+    // TODO: toggle element and run refresh
+
+   console.log("foo");
+});
+
+function refresh() {
+    //
+    // buildGraph()
+}
+
+function buildGraph(cameras, points, boxes, classmap) {
 
     // data:
     // timestamp device class confidence lat lon minx miny maxx maxy
 
-    var boxes = d3.dsvFormat("|").parseRows(raw);
-        boxesRaw = [];
-        boxes.forEach((box, index) => {
-            var coord = projection([box[5], box[4]]);
-            box[5] = coord[0];
-            box[4] = coord[1];
-            boxesRaw.push(coord);
-            return boxes[index] = box; // lonlat!
-        });
-
     console.log(boxes[0])
+
+    var boxesRaw = [];
+    boxes.forEach((box, index) => {
+        boxesRaw.push([box[5], box[4]]);
+    });
 
     var tiles = d3.tile()
         .size([width, height])
@@ -72,40 +148,29 @@ d3.text("boxes.txt", function(error, raw) {
         ();
 
     layer_map
-            .attr("class", "layer_map")
+        .attr("class", "layer_map")
         .selectAll("image")
         .data(tiles)
         .enter().append("image")
-            .attr("xlink:href", getImg)
-            .attr("filter", "url(#grayscale)")
-            .attr("x", function(d) { return (d[0] + tiles.translate[0]) * tiles.scale; })
-            .attr("y", function(d) { return (d[1] + tiles.translate[1]) * tiles.scale; })
-            .attr("width", tiles.scale)
-            .attr("height", tiles.scale);
+        .attr("xlink:href", getImg)
+        .attr("filter", "url(#grayscale)")
+        .attr("x", function(d) { return (d[0] + tiles.translate[0]) * tiles.scale; })
+        .attr("y", function(d) { return (d[1] + tiles.translate[1]) * tiles.scale; })
+        .attr("width", tiles.scale)
+        .attr("height", tiles.scale);
 
     layer_hex
-            .attr("class", "layer_hex")
+        .attr("class", "layer_hex")
         .append("g")
-            .attr("class", "hexagon")
-            .attr("clip-path", "url(#clip)")
+        .attr("class", "hexagon")
+        .attr("clip-path", "url(#clip)")
         .selectAll("path")
         .data(hexbin(boxesRaw))
         .enter().append("path")
-            .attr("d", hexbin.hexagon())
-            .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-            .attr("fill", function(d) { return color(d.length); })
-            .attr("fill-opacity", function(d) { return alpha(d.length); });
-
-    // layer_sca.append("g")
-    //     .selectAll(".dot")
-    //     .data(boxes)
-    //     .enter().append("circle")
-    //         .attr("class", "dot")
-    //         .attr("r", 0.5)
-    //         .attr("cx", function(d) { return d[5]; })
-    //         .attr("cy", function(d) { return d[4]; })
-    //         .attr("fill-opacity", 0.3)
-    //         .style("fill", "black");
+        .attr("d", hexbin.hexagon())
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+        .attr("fill", function(d) { return color(d.length); })
+        .attr("fill-opacity", function(d) { return alpha(d.length); });
 
     layer_sca.draw = function() {
         layer_sca.clearRect(0, 0, width, height);
@@ -125,17 +190,28 @@ d3.text("boxes.txt", function(error, raw) {
     };
     layer_sca.draw();
 
+    // symbols
+
+    var symbols = [];
+    cameras.forEach((cam, index) => {
+        cam["type"] = "CAMERA";
+        symbols.push(cam);
+    });
+    points.forEach((point, index) => {
+        point["type"] = "POINT";
+        symbols.push(point);
+    });
+
     var symbols = layer_sym
         .attr("class", "layer_sym")
         .selectAll(".symbol")
-        .data(symbols_data)
+        .data(symbols)
         .enter()
         .append("g")
-            .attr("transform", function(d) {
-                var coords = projection([d[2][1], d[2][0]]);
-                console.log(coords);
-                return "translate(" + coords[0] + "," + coords[1] + ")";
-            });
+        .attr("transform", function(d) {
+            var coords = projection([d["position"][1], d["position"][0]]);
+            return "translate(" + coords[0] + "," + coords[1] + ")";
+        });
 
     symbols.append("circle")
         .attr("class", "sym")
@@ -143,15 +219,23 @@ d3.text("boxes.txt", function(error, raw) {
         .attr("cx", 0)
         .attr("cy", 0)
         .attr("fill-opacity", "1")
-        .style("fill", function(d) { return d[3]; });
+        .style("fill", function(d) {
+            if (d["type"] === "CAMERA") {
+                return "red";
+            }
+            if (d["type"] === "POINT") {
+                return "grey";
+            }
+
+            return "white";
+        });
 
     symbols.append("text")
         .attr("height", 10)
         .attr("width", 10)
-        .attr("transform", "translate(" + -6 + "," + +5 + ")")
-        .text(function(d) { return d[0] });
-
-});
+        .attr("transform", "translate(" + -5.5 + "," + +5 + ")")
+        .text(function(d) { return d["type"][0] });
+}
 
 function getImg(d) {
 
