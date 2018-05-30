51fa7843057214404798ad4c94d8605c7a8b0168
Christopher Getschmann <christopher@volzo.de>
2018-05-30 02:29:35 +0200

    compressor reading works

--- Despat/app/src/main/java/de/volzo/despat/Compressor.java
+++ Despat/app/src/main/java/de/volzo/despat/Compressor.java
@@ -1,14 +1,24 @@
 package de.volzo.despat;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.media.Image;
+
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.volzo.despat.preferences.Config;
 
 /**
  * Created by volzotan on 08.03.18.
@@ -16,14 +26,51 @@ import java.io.FileOutputStream;
 
 public class Compressor {
 
-    private long[][][] mat;
+    public static final String TAG = Compressor.class.getSimpleName();
+
+//    private long[][][] mat;
+    private Mat mat;
+    private int width;
+    private int height;
 
     public Compressor() {
+        System.loadLibrary("opencv_java3");
+    }
 
+    public void test(Context context) {
+
+        File imageFolder = Config.getImageFolder(context);
+        List<File> images = new ArrayList<File>();
+
+        for (final File fileEntry : imageFolder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                continue;
+            }
+
+            if (!fileEntry.getName().toLowerCase().endsWith(".jpg")) {
+                continue;
+            }
+
+            images.add(fileEntry);
+        }
+
+        images = images.subList(0, 1);
+        Bitmap bitmap = BitmapFactory.decodeFile(images.get(0).getAbsolutePath());
+
+        init(bitmap.getWidth(), bitmap.getHeight());
+
+        for (File f : images) {
+            add(f);
+        }
+
+        toJpeg(new File(imageFolder, "compressed.jpg"));
     }
 
     public void init(int width, int height) {
-        mat = new long[width][height][3];
+        this.width = width;
+        this.height = height;
+//        mat = new long[this.width][this.height][1];
+        mat = Mat.zeros(this.height, this.width, CvType.CV_16U);
     }
 
     public void add(Image image) {
@@ -31,16 +78,9 @@ public class Compressor {
     }
 
     public void add(File path) {
-        Bitmap bitmap = BitmapFactory.decodeFile(path.getAbsolutePath());
-
-        for (int i=0; i<bitmap.getHeight(); i++) {
-            for (int j=0; j<bitmap.getWidth(); j++) {
-                int c = bitmap.getPixel(i, j);
-                mat[i][j][0] += (c >> 16) & 0xff;
-                mat[i][j][1] += (c >>  8) & 0xff;
-                mat[i][j][2] += (c      ) & 0xff;
-            }
-        }
+        Mat imgMat = Imgcodecs.imread(path.getAbsolutePath(), Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
+        Mat mask = Mat.ones(mat.height(), mat.width(), CvType.CV_8UC1);
+        Core.add(mat, imgMat, mat, mask, CvType.CV_16U);
     }
 
     public void store() {
@@ -52,13 +92,11 @@ public class Compressor {
     }
 
     public void toJpeg(File path) {
-        Bitmap bitmap = Bitmap.createBitmap(mat.length, mat[0].length, Bitmap.Config.ARGB_8888);
-        for (int i=0; i<bitmap.getHeight(); i++) {
-            for (int j=0; j<bitmap.getWidth(); j++) {
-                int c = (1 & 0xff) << 24 | ((int) mat[i][j][0] & 0xff) << 16 | ((int) mat[i][j][1] & 0xff) << 8 | ((int) mat[i][j][2] & 0xff);
-                bitmap.setPixel(i, j, c);
-            }
-        }
+        mat.convertTo(mat, CvType.CV_8UC1);
+        Mat mat2 = Mat.zeros(mat.height(), mat.width(), CvType.CV_8UC3);
+        Imgproc.cvtColor(mat, mat2, Imgproc.COLOR_GRAY2RGB);
+        Bitmap bitmap = Bitmap.createBitmap(mat2.width(), mat2.height(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(mat2, bitmap);
 
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
@@ -68,9 +106,8 @@ public class Compressor {
             FileOutputStream fileOuputStream = new FileOutputStream(path);
             fileOuputStream.write(byteArray);
             fileOuputStream.close();
-        }catch(Exception e){
+        } catch(Exception e){
             e.printStackTrace();
         }
     }
-
 }
