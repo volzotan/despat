0b59ebe3e575fc1eaff0b1005718fc1c81166190
Christopher Getschmann <christopher@volzo.de>
2018-06-25 13:45:33 +0200

    bugfix

--- evaluation/pixel_size.py
+++ evaluation/pixel_size.py
@@ -11,6 +11,7 @@ import sys
 sys.path.append('..')
 from util.converter import vocToBbox, jsonToBbox
 import numpy as np
+import matplotlib.pyplot as plt
 
 def calculate_size(box):
     x = box[2] - box[0]
@@ -30,7 +31,7 @@ if __name__ == "__main__":
 
     skipcounter_class = 0
 
-    for i in range(0, len(gt)):
+    for i in range(0, len(gt["box"])):
         if gt["class"][i] not in classes:
             skipcounter_class += 1
             continue
@@ -41,9 +42,28 @@ if __name__ == "__main__":
         ys.append(y)
         area.append(a)
 
-    print("{} bounding boxes. skipped {}/{} due class filtering".format(len(area), skipcounter_class, len(gt)))
+    print("{} bounding boxes. skipped {}/{} due class filtering".format(len(area), skipcounter_class, len(gt["box"])))
     print("x | min: {} | max: {} | mean: {}".format(np.min(xs), np.max(xs), np.mean(xs)))
     print("y | min: {} | max: {} | mean: {}".format(np.min(ys), np.max(ys), np.mean(ys)))
     print("a | min: {} | max: {} | mean: {}".format(np.min(area), np.max(area), np.mean(area)))
+
+    # Build the plot
+    plt.style.use('grayscale')
+    fig, ax = plt.subplots()
+    ax.scatter(xs, ys, s=1)
+    ax.set_ylabel('height in pixel')
+    # ax.set_xticks(x_pos)
+    # ax.set_xticklabels(xlabels)
+    ax.set_xlabel('width in pixel')
+    # ax.xaxis.grid(True)
+    # ax.yaxis.grid(True)
+
+    # Save the figure and show
+    plt.tight_layout()
+    plt.savefig('plot_pixel_size.png')
+    plt.show()
+
+    # plt.scatter(xs, ys)
+    # plt.show()
 
 
