8ea3b558a8381ca332eae5b14644ef34bba988d0
Christopher Getschmann <christopher@volzo.de>
2018-11-22 03:12:00 +0100

    minor changes

---
+++ detector/tensorboard.sh
@@ -0,0 +1,3 @@
+python3 import_pb_to_tensorboard.py                                                                                         \
+--model_dir="/Users/volzotan/GIT/despat/detector/models/ssdlite_mobilenet_v2_coco_2018_05_09/frozen_inference_graph.pb"     \
+--log_dir="tensorboard_log"
